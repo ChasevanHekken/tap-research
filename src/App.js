13e155f5a1d0801a238c7a5cf873518fd3a2262c
ChasevanHekken <cvh1717@gmail.com>
2020-12-08 11:52:12 -0500

    basic functionality without css breakpoints

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 function App() {
+  const rootURL = 'https://www.tapresearch.com/supply_api/surveys/offer';
+  const apiToken = '9a7fb35fb5e0daa7dadfaccd41bb7ad1';
+  const [invalidOffer, setInvalidOffer] = useState(null);
+  const [hasOffer, setHasOffer] = useState(null);
+  const [offerURL, setOfferURL] = useState('');
+  const [minimumReward, setMinimumReward] = useState('');
+  const [maximumReward, setMaximumReward] = useState('');
+  const [currency, setCurrency] = useState('');
+
+  function jsonp(user_identifier) {
+    const head = document.querySelector('head');
+    let script = document.createElement('script');
+    script.src = encodeURI(`${rootURL}?api_token=${apiToken}&user_identifier=${user_identifier}&callback=testCallback`);
+    script.async = true;
+
+    head.appendChild(script);
+  };
+
+  window.testCallback = response => {
+    const hasOffer = response['has_offer'];
+    setHasOffer(hasOffer);
+
+    if (hasOffer) {
+      setOfferURL(response['offer_url']);
+      setMinimumReward(response['message_hash']['min']);
+      setMaximumReward(response['message_hash']['max']);
+      setCurrency(response['message_hash']['currency']);
+    };
+  };
+
+  const handleSubmit = (event) => {
+    jsonp(event.target[0].value);
+
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    const offer = event.target.value;
+
+    if (offer.length > 32) {
+      setInvalidOffer(true);
+    } else if (offer.length < 32) {
+      setInvalidOffer(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-container">
+        {typeof(hasOffer) !== "boolean" && (
+          <Form className="survey-form" onSubmit={handleSubmit}>
+            <h3>Get Survey</h3>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Control onChange={handleChange} type="Username" placeholder="Enter username ..." />
+            </Form.Group>
+            {invalidOffer && (
+              <Form.Text className="text-muted">
+                Please enter a valid offer name less than 32 characters
+              </Form.Text>
+            )}
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        )}
+        {hasOffer && (
+          <div className="survey-form">
+            <h3>Offer Details</h3>
+            <a href={offerURL} target='_blank'>Take Survey</a>
+            <p>Minimum Reward: {minimumReward}</p>
+            <p>Maximum Reward: {maximumReward}</p>
+            <p>Currency: {currency}</p>
+          </div>
+        )}
+        {typeof(hasOffer) === "boolean" && !hasOffer && (
+          <div className="survey-form">
+            <h3>No Survey Available</h3>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
